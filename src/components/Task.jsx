6f73a163b430674562c0667e0f7aecdb031fe17f
sjmitch-git <sjmitch20@outlook.com>
2024-06-18 08:44:23 +0100

    change task background to red

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -22,6 +22,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           name="title"
           id={`title-${id}`}
           placeholder="Input title"
+          style={{ backgroundColor: "red" }}
         />
       </label>
       {state !== "TASK_ARCHIVED" && (
